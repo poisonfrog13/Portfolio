d97f9998eaf59259e70d5e6254b82da05f5a07a8
Olena Gut <lena.gut1993@gmail.com>
2024-02-27 23:25:18 +0000

    Create models

--- server/apps/portfolio/models.py
+++ server/apps/portfolio/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 
+
+
+class Projects(models.Model):
+    
+    title = models.CharField(max_length=100, null=False)
+    subtitle = models.CharField(max_length=200, null=False)
+    text = models.CharField(max_length=400, null=False)
+    image = models.ImageField(upload_to='images', null=True, blank=True)
+    icons = models.ImageField(upload_to='icons', null=True, blank=True)
+
+class Skills(models.Model):
+
+    title = models.CharField(max_length=100, null=False)
+    subtitle = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+    icons = models.ImageField(upload_to='icons', null=True, blank=True)
+
+    

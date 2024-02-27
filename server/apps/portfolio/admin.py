39dcee4cd0641646fc016fcfd7d8ee2974e0b487
Olena Gut <lena.gut1993@gmail.com>
2024-02-27 23:47:54 +0000

    Register models

--- server/apps/portfolio/admin.py
+++ server/apps/portfolio/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from server.apps.portfolio.models import Projects, Skills
 
-# Register your models here.
+admin.site.register(Projects)
+admin.site.register(Skills)

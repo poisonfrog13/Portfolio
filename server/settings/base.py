b961b9170a400dbfdbd6214d6c39d6a141562f56
Olena Gut <lena.gut1993@gmail.com>
2024-02-27 15:12:48 +0000

    Configure and launch the project

--- server/settings/base.py
+++ server/settings/base.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myportfolio.urls'
+ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myportfolio.wsgi.application'
+WSGI_APPLICATION = 'server.wsgi.application'
 
 
 # Database
